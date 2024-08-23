f2baf48aec7aabff019ccfacffd0c61bce3fe8a5
Jardrex Bolon <drex.bolon@gmail.com>
2024-08-23 12:53:55 +0800

    added app folder and routing

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-200">
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to Profile
+      </Link>
+    </View>
+  );
+}
