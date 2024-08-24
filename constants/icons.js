d48edb73dff70e4c7f107212ad89496608d8e899
Jardrex Bolon <drex.bolon@gmail.com>
2024-08-24 15:01:16 +0800

    added the bottom navigation bar

--- constants/icons.js
+++ constants/icons.js
@@ -12,7 +12,7 @@ import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
 import play from "../assets/icons/play.png";
 
-export {
+export default {
   play,
   bookmark,
   home,
