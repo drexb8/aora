ab0c654ce7a8f1388291883917896b19d742b492
Jardrex Bolon <drex.bolon@gmail.com>
2024-08-27 08:29:27 +0800

    add the functionality of sign-up and sign-up and session

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -13,7 +14,26 @@ const SignUp = () => {
     password: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const submit = () => {};
+
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      //set to global state using context api
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <SafeAreaView className="h-full bg-primary">
       <ScrollView>
--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, FlatList } from "react-native";
 import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => {
+          <Text className="text-3xl">{item.id}</Text>;
+        }}
+      />
+    </SafeAreaView>
   );
 };
 
